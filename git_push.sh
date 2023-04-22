dcc27dc476b2563d583fa5c60787fc8381f18d3b
name_tbd <email_tbd@email_provider_tbd.com>
2023-04-22 17:18:16 -0400

    commit_summary

--- git_push.sh
+++ git_push.sh
@@ -1,5 +1,8 @@
+echo Enter the commit summary:
+read commit_summary
+
 git add .
-git commit -m "Add existing file2"
+git commit -m commit_summary
 #git remote add origin git@github.com:D6AD3C/blender_python_suite.git
 git push -u origin main
 #git remote add $repo_id https://github.com/D6AD3C/blender_python_suite2.git
