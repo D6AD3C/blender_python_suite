b68fc37fd314cf6d5be457b0a07adb1a581acbac
Alex Easystreet <alex.easystreet@gmail.com>
2023-04-22 17:02:33 -0400

    Add existing file2

---
+++ git_push.sh
@@ -0,0 +1,6 @@
+git add .
+git commit -m "Add existing file2"
+#git remote add origin git@github.com:D6AD3C/blender_python_suite.git
+git push -u origin main
+#git remote add $repo_id https://github.com/D6AD3C/blender_python_suite2.git
+
